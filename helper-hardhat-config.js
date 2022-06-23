e22274faa5012084b08d2f0941c7f796b227ce3f
Nedelin Slavov <nedelin.slavov@gmail.com>
2022-06-23 18:05:00 +0300

    Added block confirmations constant

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -21,8 +21,10 @@ const networkConfig = {
 }
 
 const developmentChains = ["hardhat", "localhost"]
+const VERIFICATION_BLOCK_CONFIRMATIONS = 6
 
 module.exports = {
     networkConfig,
     developmentChains,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
 }

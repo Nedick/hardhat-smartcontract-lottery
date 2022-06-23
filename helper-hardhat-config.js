f86c3a966494c2f2f06d5822e00e87d5947b5b99
Nedelin Slavov <nedelin.slavov@gmail.com>
2022-06-23 18:34:44 +0300

    Fixed the name of the coordinator contract

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,10 +1,8 @@
-const { ethers } = require("hardhat")
-
 const networkConfig = {
     4: {
         name: "rinkeby",
-        vrfCoordinator: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
-        entranceFee: ethers.utils.parseEther("0.01"),
+        vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
+        entranceFee: "100000000000000000", // 0.1 ETH
         gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
         subscriptionId: "7033", // subscription id of the Chainlink VRF contract
         // needed to setup keepers for our contract!
@@ -13,7 +11,7 @@ const networkConfig = {
     },
     31337: {
         name: "hardhat",
-        entranceFee: ethers.utils.parseEther("0.01"),
+        entranceFee: "100000000000000000", // 0.1 ETH
         gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
         callbackGasLimit: "500000", // 500k
         interval: "30",
